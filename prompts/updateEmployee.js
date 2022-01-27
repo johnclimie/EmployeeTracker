e0026e238c0dae09304841d48edf70629708661b
John Climie <johnclimie00@gmail.com>
2022-01-27 18:47:56 -0500

    Update Employee function properly works

--- prompts/updateEmployee.js
+++ prompts/updateEmployee.js
@@ -4,23 +4,67 @@ const db = require('../connection').connection;
 const index = require('../index');
 
 const updateEmp = () => {
-    inquirer
-        .prompt([
-            {
-                name: 'empName',
-                message: 'Which employee would you like to update?',
-                type: 'input'
-            },
-            {
-                name: 'newRole',
-                message: `Enter a new role for this employee`,
-                type: 'input'
+    db.query(
+        `SELECT last_name
+         FROM employee;`, function(err, results) {
+            if (err) {
+                console.log(err);
+            } else {
+                let arrLast = [];
+                results.forEach(person => arrLast.push(`${person.last_name}`));
+                
+
+                db.query(
+                    `SELECT first_name
+                     FROM employee;`, function(err, results) {
+                         if (err) {
+                             console.log(err);
+                         } else {
+                             let arrFirst = [];
+                             results.forEach(person => arrFirst.push(`${person.first_name}`))
+
+                             inquirer
+                             .prompt([
+                                 {
+                                     name: 'lName',
+                                     message: 'What is the last name of the employee you would like to update?',
+                                     type: 'list',
+                                     choices: arrLast
+                                 },
+                                 {
+                                     name: 'fName',
+                                     message: 'What is the first name of the employee you would like to update?',
+                                     type: 'list',
+                                     choices: arrFirst
+                                 },
+                                 {
+                                     name: 'newRole',
+                                     message: `Enter a new role id for this employee`,
+                                     type: 'input'
+                                 }
+                             ])
+                             .then(function(response) {
+                                 db.query(
+                                     `UPDATE employee
+                                      SET role_id = ${response.newRole}
+                                      WHERE first_name = '${response.fName}' AND last_name = '${response.lName}'`,
+                                    function(err) {
+                                        if (err) {
+                                            console.log(err);
+                                        }
+                                    }
+                                 )
+                             })
+                             .then(() => {
+                                 console.log("Update was successful \n")
+                                 index.makeSelection()
+                             });
+                         }
+                     }
+                )
             }
-        ])
-        .then(function () {
-            console.log('employee updated');
-            index.makeSelection();
-        });
+         }
+    )
 }
 
 exports.updateEmp = updateEmp;
